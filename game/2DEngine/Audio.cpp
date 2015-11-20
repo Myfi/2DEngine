d56957a605058dbcc70f66485b910256924173b8
ndzurche <ndzurche@mtu.edu>
2015-11-20 11:50:30 -0600

    Finally committing audio files

---
+++ game/2DEngine/Audio.cpp
@@ -0,0 +1,65 @@
+#include "Audio.h"
+
+Audio::Audio()
+{
+	//must call this for COM
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+
+	//create the engine
+	if (FAILED(XAudio2Create(&g_engine)))
+	{
+		CoUninitialize();
+		return;
+	}
+
+	//create the mastering voice
+	if (FAILED(g_engine->CreateMasteringVoice(&g_master)))
+	{
+		g_engine->Release();
+		CoUninitialize();
+		return;
+	}
+
+	//helper class to load wave files; trust me, this makes it MUCH easier
+	Wave buffer;
+}
+
+void Audio::playSound(char c[])
+{
+	//load a wave file
+	if (!buffer.load(c))
+	{
+		g_engine->Release();
+		CoUninitialize();
+		return;
+	}
+
+	//create the source voice, based on loaded wave format
+	if (FAILED(g_engine->CreateSourceVoice(&g_source, buffer.wf())))
+	{
+		g_engine->Release();
+		CoUninitialize();
+		return;
+	}
+
+	//start consuming audio in the source voice
+	g_source->Start();
+
+	//play the sound
+	g_source->SubmitSourceBuffer(buffer.xaBuffer());
+
+}
+
+void Audio::release()
+{
+	//release the engine, NOT the voices!
+	g_engine->Release();
+
+	//again, for COM
+	CoUninitialize();
+
+	return;
+}
+
+
+
---
+++ game/2DEngine/Audio.h
@@ -0,0 +1,24 @@
+#ifndef _AUDIO_H
+#define _AUDIO_H
+
+#include <xaudio2.h>
+#include "wave.h"
+
+class Audio
+{
+	private:
+		Wave buffer;
+		IXAudio2* g_engine;
+		IXAudio2SourceVoice* g_source;
+		IXAudio2MasteringVoice* g_master;
+	public:
+
+	Audio(); //Constructor
+
+	virtual void release(); //Release
+
+	virtual void playSound(char c[]); //Play the sound
+};
+
+#endif
+
