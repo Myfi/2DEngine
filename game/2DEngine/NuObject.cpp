15a8a4976093d3665b9f82d21e05a7b9366ccf21
Colin <cmhogue@mtu.edu>
2015-12-06 20:16:40 -0500

    changes

---
+++ game/2DEngine/NuObject.cpp
@@ -0,0 +1,19 @@
+#include "NuObject.h"
+
+
+
+NuObject::NuObject(HWND parent, LPCSTR title, int x, int y)
+{
+	//name = title;
+	name = title;
+	reference = CreateWindow(
+		TEXT("BUTTON"),                 /* Class Name */
+		title,							/* Title */
+		WS_VISIBLE | WS_CHILD,          /* Style */
+		x, y,							/* Position */
+		80, 20,							/* Size */
+		parent,							/* Parent */
+		(HMENU)ID_DATA,                 /* Menu type */
+		NULL,                           /* Instance */
+		0);
+}
